a459a1c8da783e3aad7f99c338d00d4ca447aa20
Olivier Labayle <olabayle@gmail.com>
2023-11-30 18:24:21 +0000

    add more commonicon stuff

---
+++ deps/execute.jl
@@ -0,0 +1,7 @@
+using TargetedEstimation
+
+TargetedEstimation.command_main(["-h"])
+TargetedEstimation.command_main(["tmle", "-h"])
+TargetedEstimation.command_main(["make-summary", "-h"])
+TargetedEstimation.command_main(["sieve-variance-plateau", "-h"])
+
