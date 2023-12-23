4b0dcd6e6b34b7bb3d103c0b9c5c58aa9e0278c0
Olivier Labayle <olabayle@gmail.com>
2023-12-23 10:44:45 +0100

    fix executable name

--- deps/build_app.jl
+++ deps/build_app.jl
@@ -1,6 +1,6 @@
 using PackageCompiler
-PackageCompiler.create_app(".", "tmle", 
-    lib_name="tmle",
+PackageCompiler.create_app(".", "tmle",
+    executables = ["tmle" => "julia_main"]
     precompile_execution_file="deps/execute.jl", 
     include_lazy_artifacts=true
 )
