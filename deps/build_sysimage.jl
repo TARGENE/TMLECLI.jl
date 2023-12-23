6ddceaecad6bc44a905e10df8200ca4da6c8915e
Olivier Labayle <olabayle@gmail.com>
2023-12-23 11:15:26 +0100

    try generic cpu target

--- deps/build_sysimage.jl
+++ deps/build_sysimage.jl
@@ -1,6 +1,7 @@
 using PackageCompiler
 PackageCompiler.create_sysimage(
     ["TargetedEstimation"], 
+    cpu_target="generic",
     sysimage_path="TMLESysimage.so", 
     precompile_execution_file="deps/execute.jl", 
 )
